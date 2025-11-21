207624655cff200d71720cb2153c1ad179a22d27
crazyro6 <roypara@icloud.com>
2025-11-21 15:41:19 +0000

    Descripción del cambio realizado

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
 public static void main(String[] args) {
-System.out.println("Hola Mundo, este es un cambio, y este es otro, otro mas");
+System.out.println("Hola Mundo, este es un cambio, y este es otro, otro mas y otro");
 }
 }
